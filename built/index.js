da374c156d541d0284530de8bf66edecf7b02c39
Le Val <adelei@protonmail.com>
2021-09-30 22:21:15 -0500

    removed compiled code

--- built/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const discord_js_1 = require("discord.js");
-require("dotenv/config");
-const manager = new discord_js_1.ShardingManager('./built/bot.js', { token: process.env.TOKEN, mode: 'worker' });
-manager.on('shardCreate', shard => console.log('\x1b[32m%s\x1b[0m', `Launched shard ${shard.id}`));
-manager.spawn({ amount: 'auto' });
--- built/options.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var options;
-(function (options) {
-    options["owner"] = "830318264907202561";
-    options["prefix"] = "!";
-})(options || (options = {}));
-module.exports = options;
